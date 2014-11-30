a499d6d363ace40146b9550d559b0445e5ed7939
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-30 20:55:50 +0000

    notes added

--- lib/euler2.rb
+++ lib/euler2.rb
@@ -11,3 +11,5 @@ def add_next_finonacci_number
   end
   fib_series
 end
+
+(1..32).inject([0,1]) {|arr, i| (arr << arr[-1] + arr[-2] if arr[-1] + arr[-2] <= 4000000) || arr}.inject(0) {|total, i| total += i.even? ? i : 0}
