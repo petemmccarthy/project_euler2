a215bcbaad9aca3cda462db5f28762af7aa0ff9a
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-12-02 20:05:30 +0000

    method updated

--- lib/euler2.rb
+++ lib/euler2.rb
@@ -3,13 +3,5 @@ def sum_two_numbers(num1, num2)
 end
 
 def add_next_finonacci_number
-  fib_series = [1, 2]
-  while fib_series < 1000
-    fib_series = [1, 2]
-    fib_series[-1], fib_series[-2] == num1, num2
-    fib_series << sum_two_numbers(fib_series[-1], fib_series[-2])
-  end
-  fib_series
+  
 end
-
-(1..32).inject([0,1]) {|arr, i| (arr << arr[-1] + arr[-2] if arr[-1] + arr[-2] <= 4000000) || arr}.inject(0) {|total, i| total += i.even? ? i : 0}
